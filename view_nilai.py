f6a5516ac334e17f85403e6bcd6c399e0cdf0f6b
Dendy Saputra <dendyscik@gmail.com>
2022-01-16 07:06:01 +0700

    Menambahkan file /c/Users/acer/UAS/view_nilai.py

---
+++ view_nilai.py
@@ -0,0 +1,51 @@
+from model.daftar_nilai import kontak
+
+def header():
+    header="PROGRAM INPUT NILAI MAHASISWA"
+    header2=("MENU UTAMA")
+    print(header.center(97,"="))
+    print()
+    print(header2.center(97,"_"))
+    c = input("\n(L)ihat, (T)ambah, (U)bah), (H)apus, (C)ari, (K)eluar: ")
+
+
+def notoption():
+    c = input("\n(L)ihat, (T)ambah, (U)bah), (H)apus, (C)ari, (K)eluar: ")
+    
+def cetak():
+    print("="*78)
+            print("|                               Daftar Mahasiswa                             |")
+            print("="*78)
+            print("|No. | Nama            |       NIM       |  UTS  |  UAS  |  Tugas  |  Akhir  |")
+            print("="*78)
+            i = 0
+            for z in data.items():
+                i += 1
+                print("| {no:2d} | {0:15s} | {1:15d} | {2:5d} | {3:5d} | {4:7d} | {5:7.2f} |"
+                      .format(z[0][:13], z[1][0], z[1][1], z[1][2], z[1][3], z[1][4], no=i))
+            print("=" * 78)
+        else:
+            print("="*78)
+            print("|                               Daftar Mahasiswa                             |")
+            print("="*78)
+            print("|No. | Nama            |       NIM       |  UTS  |  UAS  |  Tugas  |  Akhir  |")
+            print("="*78)
+            print("|                                TIDAK ADA DATA                              |")
+            print("="*78)
+
+
+def cari():
+    from view.input_nilai import cari
+    print("Cari Data[case-sensitive]")
+        nama = input("Masukkan Nama : ")
+        if nama in data.keys():
+            print("="*73)
+            print("|                             Daftar Mahasiswa                          |")
+            print("="*73)
+            print("| Nama            |       NIM       |  UTS  |  UAS  |  Tugas  |  Akhir  |")
+            print("="*73)
+            print("| {0:15s} | {1:15d} | {2:5d} | {3:5d} | {4:7d} | {5:7.2f} |"
+                  .format(nama, nim, uts, uas, tugas, akhir))
+            print("="*73)
+        else:
+            print("Nama {0} Tidak Ditemukan".format(nama)) 
