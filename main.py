0ec64fe67bbdcebee08cabb4909b16bf02cf5e13
Dendy Saputra <dendyscik@gmail.com>
2022-01-16 07:11:43 +0700

    Menambahkan file /c/Users/acer/UAS/main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from view.view_nilai import cari,cetak,header,notoption
+from view.input_nilai import inputan,ubah,hapus
+header()
+while True:
+
+    c = input("\nPilih Opsi: ")
+
+    # EXIT PROGRAM
+    elif c. lower() == 'k':
+        break
+    
+    # PRINT DATA
+    elif c.lower() == 'l':
+        if data.items():
+
+    # MENAMBAH DATA
+      if c.lower() == 't':
+        print("Tambah Data")
+
+    # UBAH DATA
+    elif c.lower() == 'u':
+        print("Ubah Data")
+
+    # MENCARI DATA
+    elif c.lower() == 'c':
+        print("Cari Data[case-sensitive]")
+
+    # HAPUS DATA
+    elif c.lower() == 'h':
+        print("Hapus Data")
+        
+    else:
+        notoption()
